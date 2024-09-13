90fcabbe4221c98779bbd4ff3189d538528c3e80
ThomasEcalle <thomasecalle@gmail.com>
2024-09-13 10:24:39 +0200

    Add futures chapter

---
+++ lib/futures/app_exception.dart
@@ -0,0 +1,5 @@
+abstract class AppException implements Exception {}
+
+class StringValueException extends AppException {}
+
+class LengthValueException extends AppException {}
--- lib/futures/futures.dart
+++ lib/futures/futures.dart
@@ -1,27 +1,42 @@
-void main() {
+import 'package:al2_2024/futures/app_exception.dart';
+
+void main() async {
   print('1');
 
-  getStringFromNetwork().then((value) {
-    getLengthFromNetwork(value).then((length) {
-      print('Length = $length');
-    }).catchError((error) {
-      print('une erreur est survenue : $error');
-    });
-  }).catchError((error) {
+  try {
+    final value = await getStringFromNetwork();
+    final length = await getLengthFromNetwork(value);
+    print('Length = $length');
+  } on StringValueException catch (error) {
+    print('Erreur dans la récupération de la string: $error');
+  } on LengthValueException catch (error) {
+    print('Erreur dans la récupération de la length: $error');
+  } catch (error) {
     print('une erreur est survenue : $error');
-  });
+  } finally {
+    print('3');
+  }
 
-  print('3');
+  // getStringFromNetwork().then((value) {
+  //   getLengthFromNetwork(value).then((length) {
+  //     print('Length = $length');
+  //     print('3');
+  //   }).catchError((error) {
+  //     print('une erreur est survenue : $error');
+  //   });
+  // }).catchError((error) {
+  //   print('une erreur est survenue : $error');
+  // });
 }
 
-Future<String> getStringFromNetwork() {
-  return Future.delayed(const Duration(seconds: 1), () {
-    return 'Coucou';
-  });
+Future<String> getStringFromNetwork() async {
+  await Future.delayed(const Duration(seconds: 1));
+
+  return 'Coucou';
 }
 
-Future<int> getLengthFromNetwork(String value) {
-  return Future.delayed(const Duration(seconds: 1), () {
-    return value.length;
-  });
+Future<int> getLengthFromNetwork(String value) async {
+  await Future.delayed(const Duration(seconds: 1));
+  throw LengthValueException();
+  return value.length;
 }
